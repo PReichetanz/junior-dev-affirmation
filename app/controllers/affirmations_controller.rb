248b0a8b9f05e22a375dba54c8c3c119a0699c4f
Paul Reichetanz <paul@reichetanz.de>
2024-11-07 11:06:52 +0100

    add detail page

--- app/controllers/affirmations_controller.rb
+++ app/controllers/affirmations_controller.rb
@@ -2,4 +2,7 @@ class AffirmationsController < ApplicationController
   def index
     @affirmations = Affirmation.all
   end
+  def show
+    @affirmation = Affirmation.find(params[:id])
+  end
 end
